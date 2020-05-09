ee856c68b4964af9ca702cc281240966580985f5
vn32 <nishadvikas87@gmial.com>
2020-05-09 12:14:51 +0530

    Implemeneted PermisionUtils into MapsActivity

--- app/src/main/java/com/vikas/ridesharing/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/vikas/ridesharing/ui/maps/MapsActivity.kt
@@ -1,6 +1,8 @@
 package com.vikas.ridesharing.ui.maps
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Transformations.map
 import com.google.android.gms.maps.GoogleMap
@@ -8,12 +10,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.vikas.ridesharing.R
 import com.vikas.ridesharing.data.network.NetworkService
+import com.vikas.ridesharing.utils.PermissionUtils
 import com.vikas.ridesharing.utils.ViewUtils
 
 class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
     companion object{
         private const val TAG="MapsActivity"
     }
+    private val LOCATION_PERMISSION_REQUEST_CODE=999
     private lateinit var presenter: MapsPresenter
     private lateinit var mMap: GoogleMap
 
@@ -31,5 +35,54 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+    //to check whether permission is given or not for location
+    override fun onStart() {
+        super.onStart()
+        when{
+            PermissionUtils.isAccessFineLocationGranted(this)->{
+                when {
+                    PermissionUtils.isLocationEnabled(this) -> {
+                        //fetch the location
+                    } else -> {
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                }
+
+            } else ->{
+            PermissionUtils.requestAccessFineLocationPermission(this,LOCATION_PERMISSION_REQUEST_CODE)
+                }
+        }
+    }
+    //checking whether permision for location access is granted or not
+    //if permission granted then checking whether gpslocationenable or not
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            LOCATION_PERMISSION_REQUEST_CODE ->{
+                if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    when {
+                        PermissionUtils.isLocationEnabled(this) -> {
+                            //fetch the location
+                        } else -> {
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                    }
+                } else {
+                    Toast.makeText(this,"Location Permission is not granted",Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+        presenter.onDetach()//to disconect from networkservice before app destroy
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/vikas/ridesharing/utils/PermissionUtils.kt
+++ app/src/main/java/com/vikas/ridesharing/utils/PermissionUtils.kt
@@ -1,4 +1,40 @@
 package com.vikas.ridesharing.utils
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 object PermissionUtils {
+    fun requestAccessFineLocationPermission(activity:AppCompatActivity,requestId:Int){
+        ActivityCompat.requestPermissions(activity,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),requestId)
+    }
+    //function to check permission granted or not
+    fun isAccessFineLocationGranted(context: Context):Boolean{
+        return ContextCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED
+
+    }
+    //check if location is enable or not, else showGpsEnable dialog
+    fun isLocationEnabled(context: Context):Boolean{
+        val locationManager:LocationManager=context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+    fun showGPSNotEnabledDialog(context: Context){
+        AlertDialog.Builder(context)
+            .setTitle("Enabled GPS")
+            .setMessage("Required for this app")
+            .setCancelable(false)
+            .setPositiveButton("Enabled Now"){_,_->
+                context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+
+            }
+            .show()
+    }
 }
