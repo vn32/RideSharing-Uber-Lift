1564583fdfc7d0abfa3737049e1b391615d27af2
vn32 <nishadvikas87@gmial.com>
2020-05-10 00:31:45 +0530

    added autocompleteactivity and onresultactivty

--- app/src/main/java/com/vikas/ridesharing/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/vikas/ridesharing/ui/maps/MapsActivity.kt
@@ -1,11 +1,15 @@
 package com.vikas.ridesharing.ui.maps
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Transformations.map
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -15,16 +19,23 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.vikas.ridesharing.R
 import com.vikas.ridesharing.data.network.NetworkService
 import com.vikas.ridesharing.utils.MapUtils
 import com.vikas.ridesharing.utils.PermissionUtils
 import com.vikas.ridesharing.utils.ViewUtils
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
     companion object{
         private const val TAG="MapsActivity"
         private const val LOCATION_PERMISSION_REQUEST_CODE=999
+        private const val PICKUP_REQUEST_CODE=1
+        private const val DROP_REQUEST_CODE=2
     }
     //for showing cabs on view
     private val nearByCabsMarkerList= arrayListOf<Marker>()
@@ -32,7 +43,9 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
     private var fusedLocationProviderClient:FusedLocationProviderClient?=null
     private lateinit var locationCallback: LocationCallback   //variable for location access
     private var currentLatLng: LatLng?=null   //variable for location access
-
+    //for pickup and drop location
+    private var pickUpLatLng:LatLng?=null
+    private var dropLatLng:LatLng?=null
     lateinit var presenter: MapsPresenter
     private lateinit var googleMap: GoogleMap
 
@@ -46,7 +59,25 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
         //initializing presenter
         presenter= MapsPresenter(NetworkService())
         presenter.onAttach(this)//here this means MapsActivity(MapsView)
+        setUpClickListener()
     }
+    //for accessing current and drop location and adding clicklistener to layout file
+    private fun setUpClickListener() {
+        pickUpTextView.setOnClickListener{
+        launchLocationAutoCompleteActivity(PICKUP_REQUEST_CODE)
+        }
+       dropTextView.setOnClickListener{
+        launchLocationAutoCompleteActivity(DROP_REQUEST_CODE)
+        }
+    }
+    //Think???
+    private fun launchLocationAutoCompleteActivity(requestCode: Int){
+        val fields:List<Place.Field> = listOf(Place.Field.ID,Place.Field.NAME,Place.Field.LAT_LNG)
+        val intent= Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fields).build(this)
+        startActivityForResult(intent,requestCode)
+        //requestcode for checking which response is coming either pickup or droptextview
+    }
+
     //move camera at current LatLng
     private fun moveCamera(latLng: LatLng?){
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
@@ -64,6 +95,11 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
         val bitmapDescriptor=BitmapDescriptorFactory.fromBitmap(MapUtils.getCarBitMap(this))
         //setting this images to cabs location in view
         return googleMap.addMarker(MarkerOptions().position(latLng).flat(true).icon(bitmapDescriptor))
+    }
+    //for showing current location in layout
+    private fun setCurrentLocationAsPick(){
+        pickUpLatLng=currentLatLng
+        pickUpTextView.text=getString(R.string.current_location)
     }
 
     //to show myself on location area
@@ -84,6 +120,7 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
                     for(location in locationResult.locations){
                         if(currentLatLng==null){
                             currentLatLng= LatLng(location.latitude,location.longitude)
+                            setCurrentLocationAsPick()
                             enableMyLocationOnMap()
                             moveCamera(currentLatLng)
                             animateCamera(currentLatLng)
@@ -151,6 +188,36 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
 
         }
     }
+    //we are getting data from autocompleteactivity inside the onactivityresult
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== PICKUP_REQUEST_CODE || requestCode== DROP_REQUEST_CODE){
+            when(resultCode){
+                Activity.RESULT_OK -> {
+                    val place=Autocomplete.getPlaceFromIntent(data!!)//data receive in parameter
+                    when(requestCode){
+                        PICKUP_REQUEST_CODE -> {
+                            pickUpTextView.text=place.name
+                            pickUpLatLng=place.latLng//storing picup loaction inside the variable
+                        }
+                        DROP_REQUEST_CODE -> {
+                            dropTextView.text=place.name
+                            dropLatLng=place.latLng//storing drop location inside the varibale
+                        }
+
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    val status: Status=Autocomplete.getStatusFromIntent(data!!)
+                    Log.d(TAG,status.statusMessage)
+                }
+                Activity.RESULT_CANCELED -> {
+                    //logging
+                }
+            }
+        }
+    }
+
 
     override fun onDestroy() {
         presenter.onDetach()//to disconect from networkservice before app destroy
